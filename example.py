3496a635b3e63aeb818e9d9151420e75d89e4cda
Ilya Boyur <ilya.boyur.r@ya.ru>
2023-03-20 10:58:34 +0300

    Refactor FactoryFileBuilder result in list

--- example.py
+++ example.py
@@ -1,7 +1,9 @@
 import argparse
 
 from factory_builder import FactoryFileBuilder
-from test_builders import TestImportsBuilder, TestViewsetBuilder
+from viewset_builders import TestViewsetImportsBuilder, TestViewsetBuilder
+from routes_builder import TestRouteImportsBuilder, TestRouteBuilder
+from constants import SUPPORTED_TESTS
 
 
 if __name__ == "__main__":
@@ -11,25 +13,33 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--factories", default="")
     args = parser.parse_args()
 
-    models_file = args.models 
+    models_file = args.models or "models.txt"
     factories_file = args.factories or "out/factories.py"
     test_view_file = args.test_views or "out/test_viewsets.py"
 
     MODELS = open(models_file).readlines() if models_file else []
     
-    with open(factories_file, "w"):
-        pass
-    builder = FactoryFileBuilder(None, models=MODELS, outfile=factories_file)
+    builder = FactoryFileBuilder(None, models=MODELS)
     builder.build()
+    with open(factories_file, "w") as file:
+        file.writelines(builder.out)
+
 
     with open(test_view_file, "w"):
         pass
-    builder = TestImportsBuilder(MODELS, outfile=test_view_file)
+    builder = TestViewsetImportsBuilder(MODELS, outfile=test_view_file)
     builder.dump_test_imports()
 
     builder = TestViewsetBuilder(None, outfile=test_view_file)
     for model in MODELS:
         builder.build_schema(model)
-        builder.dump_test_class(("list","detail",))
+        builder.dump_test_class(methods=SUPPORTED_TESTS)
+    # builder = TestRouteImportsBuilder(["Action"], outfile="out/test_routes_TEMP.py")
+    # builder.build()
 
-
+    # builder = TestRouteBuilder(
+    #     ["actions", "actions", SUPPORTED_TESTS],
+    #     outfile="out/test_routes_TEMP.py"
+    # )
+    # builder.build()
+    
